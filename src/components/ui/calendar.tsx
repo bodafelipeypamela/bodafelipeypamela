310eb4f4cb95eae918ff2127d513676c62784999
bodafelipeypamela <bodafelipeypamela@gmail.com>
2026-01-29 22:19:25 -0600

    Update calendar.tsx

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,12 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        IconPrevious: () => <ChevronLeft className="h-4 w-4" />,
+        IconNext: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
