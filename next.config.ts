2e5e6092c09680e7a7fc22931e6a1c357a0c6d7f
bodafelipeypamela <bodafelipeypamela@gmail.com>
2026-02-03 13:42:05 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type {NextConfig} from 'next';
 
 const isProd = process.env.NODE_ENV === 'production';
+const repoName = '/bodafelipeypamela';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/bodafelipeypamela' : '',
-  assetPrefix: isProd ? '/bodafelipeypamela' : '',
+  // basePath le dice a Next.js que la app vive en esa subcarpeta
+  basePath: isProd ? repoName : '',
+  // assetPrefix ayuda con los archivos de sistema (_next/...)
+  assetPrefix: isProd ? repoName : '',
   typescript: {
     ignoreBuildErrors: true,
   },
