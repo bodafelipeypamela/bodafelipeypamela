a1aaa0c193993317ab24b829f5d84e7393604388
bodafelipeypamela <bodafelipeypamela@gmail.com>
2026-02-03 13:26:21 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: isProd ? '/bodafelipeypamela' : '',
-  assetPrefix: isProd ? '/bodafelipeypamela/' : '',
+  assetPrefix: isProd ? '/bodafelipeypamela' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
